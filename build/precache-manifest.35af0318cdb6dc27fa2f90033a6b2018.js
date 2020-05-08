b434d8218e578061b4942bb85f55b49d4f8e4fc5
krabaton <krabatua@gmail.com>
2020-05-08 13:43:43 +0300

    add Demo

---
+++ build/precache-manifest.35af0318cdb6dc27fa2f90033a6b2018.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4a161376846acd7f3b93de605dcd45d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "945718c51de6f7b4b631",
+    "url": "./static/css/main.a8b38e31.chunk.css"
+  },
+  {
+    "revision": "53bc611396a6a301afe7",
+    "url": "./static/js/2.7de1fbf5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.7de1fbf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "945718c51de6f7b4b631",
+    "url": "./static/js/main.5e4d4bb3.chunk.js"
+  },
+  {
+    "revision": "2821f57921e6593a5d7e",
+    "url": "./static/js/runtime-main.7a755bc1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
