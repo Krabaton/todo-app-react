bb299be889ade38f9667fa285686617b2fd9e99f
krabaton <krabatua@gmail.com>
2020-07-27 01:12:01 +0300

    build

---
+++ build/precache-manifest.9f1c513e5bfede242b08fd02daf87248.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69ea9c923881d8de5499114e41f3e69d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f1e47cc1ce4169a8cc5b",
+    "url": "./static/css/main.55f3d1eb.chunk.css"
+  },
+  {
+    "revision": "27adeb91034e78c9bd53",
+    "url": "./static/js/2.0c3bc6e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0c3bc6e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1e47cc1ce4169a8cc5b",
+    "url": "./static/js/main.d383216b.chunk.js"
+  },
+  {
+    "revision": "2821f57921e6593a5d7e",
+    "url": "./static/js/runtime-main.7a755bc1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
